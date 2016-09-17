81a7a20b990c5e5d99e46304e1b791a089de21b7
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-09-17 01:03:43 -0400

    Finished site- animated subheader

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,20 @@ jQuery(document).ready(function(){
   $(".tower-button").mouseleave(function(){
     $(".subhead").removeClass("subhead-preview");
   });
+  
+  $(function() {
+    $('a[href^="#"]').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 100
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 });
