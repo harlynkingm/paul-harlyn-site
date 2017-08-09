b1d56f0792585f566a73a9e3bc68b5229936be75
Max Harlynking <mharlynk@andrew.cmu.edu>
2017-08-09 17:18:16 -0400

    added time display

--- js/scripts.js
+++ js/scripts.js
@@ -30,23 +30,29 @@ jQuery(document).ready(function(){
   
   var sc_widget = SC.Widget("sc_player");
   
-  sc_widget.bind("ready", function(){
+  sc_widget.bind(SC.Widget.Events.READY, function(){
     updateTitle();
     
-    sc_widget.bind("play", function(){
+    sc_widget.bind(SC.Widget.Events.PLAY, function(){
       updateTitle();
       $('#play-button').attr('src', 'images/buttons/pause_button.png');
     });
     
-    sc_widget.bind("pause", function(){
+    sc_widget.bind(SC.Widget.Events.PAUSE, function(){
       updateTitle();
       $('#play-button').attr('src', 'images/buttons/play_button.png');
     });
     
+    sc_widget.bind(SC.Widget.Events.FINISH, function(){
+      next();
+    });
+    
+    sc_widget.bind(SC.Widget.Events.PLAY_PROGRESS, function(){
+      updatePosition();
+    });
+    
     $('#ff-button').click(function(){
-      sc_widget.next();
-      sc_widget.pause();
-      updateTitle();
+      next();
     });
     
     $('#play-button').click(function(){
@@ -59,6 +65,58 @@ jQuery(document).ready(function(){
     sc_widget.getCurrentSound(function(e){
       $('#sc_song').html("<a href='" + e.permalink_url + "' target='_blank'>" + e.title + "</a>");
       $("#sc_link").html("<a href='" + e.permalink_url + "' target='_blank'>soundcloud</a>");
+      updatePosition();
+      updateDuration();
+    });
+  }
+  
+  function updatePosition(){
+    sc_widget.getPosition(function(position){
+      var realPosition = new Date(position);
+      $("#sc_position").text(formatTime(realPosition) + " ");
+    });
+  }
+  
+  function updateDuration(){
+    sc_widget.getDuration(function(duration){
+      var realDuration = new Date(duration);
+      $("#sc_duration").text("/ " + formatTime(realDuration) + " | ");
+    });
+  }
+  
+  function formatTime(d){
+    return d.getMinutes() + ":" + formatNumber(d.getSeconds());
+  }
+  
+  function formatNumber(n){
+    if (n < 10){
+      return "0" + n;
+    } else {
+      return "" + n;
+    }
+  }
+  
+  $(".changeLink").click(function(){
+    var url = $(this).data("url");
+    changeSong(url);
+  });
+  
+  function changeSong(newUrl){
+    sc_widget.pause();
+    sc_widget.load(newUrl, {"auto_play": true, "hide_related":false, "show_comments":false, "show_user":true, "visual":true});
+  }
+
+  function next(){
+    sc_widget.getSounds(function(e){
+      if (e.length > 1){
+        sc_widget.next();
+        updateTitle();
+      } else {
+        sc_widget.load("https://api.soundcloud.com/playlists/291200608", {"auto_play": true, "hide_related":false, "show_comments":false, "show_user":true, "visual":true}, function(e){
+          sc_widget.play();
+          updateTitle();
+        });
+      }
     });
   }
 
