5ea3393586740ed6116084792e09376d8742ab55
Max Harlynking <mharlynk@andrew.cmu.edu>
2023-09-16 15:15:24 -0400

    updates for 2023

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,10 @@ jQuery(document).ready(function(){
   });
   
   $(".content-feature").parallax({
-    imageSrc: 'images/cover2.jpg'
+    imageSrc: 'images/cover2.jpg',
+    // imageSrc: 'images/cover3.jpg',
+    // positionY: '-160px',
+    // positionX: '0px',
   });
 
 });
